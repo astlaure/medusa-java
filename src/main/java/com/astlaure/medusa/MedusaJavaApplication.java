3b1821ee7a2cfe6b85aa8a51d6bf84967d5f2928
Alexandre St-Laurent <alexandre.stlaurent.4@gmail.com>
2024-08-04 08:22:47 -0400

    generated project

---
+++ src/main/java/com/astlaure/medusa/MedusaJavaApplication.java
@@ -0,0 +1,13 @@
+package com.astlaure.medusa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedusaJavaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MedusaJavaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/astlaure/medusa/MedusaJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.astlaure.medusa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedusaJavaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
